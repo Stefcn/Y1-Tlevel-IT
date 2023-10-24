05cc30eda96f5bd24268f496d123d1bc61c3a4c2
Stefcn <148072281+Stefcn@users.noreply.github.com>
2023-10-24 10:14:36 +0100

    Add files via upload

---
+++ DINO QUESTIONS.py
@@ -0,0 +1,32 @@
+answer1 = str(input("is the dinasaur a herbivore? (YES/NO highercase answers) "))
+if answer1 == "YES":
+    answer2 = str(input("does the dinasaur have long necks"))
+else: 
+    print("your dinasaur is brachiosaurus")
+if answer2 =="no"
+    answer3 = str(input("does it have horns"))
+else: 
+    print("your dinasaur is a triceratops")
+if answer3 =="no":
+    answer4 = str(input("is it a carnivore"))
+else:
+    print("your dinasaur cant exit")
+if answer4 =="no":
+    answer5 = str(input("is it a omnivore"))
+else:
+    print ("gallimimus")
+if answer5 =="no":
+    print("does not exist")
+    answer6 = str(input("is it the fastest"))
+else:
+     print("nanotyrannasaurus")
+if answer6 =="no":
+    answer7 = str(input("does it live in the sea"))
+else:
+    print("liopleurodon")
+if answer7 =="no":
+    answer8 =str(input("is it the biggest"))
+else: 
+    print("spinosaurus")
+if answer8 =="no":
+    print("dinasaur cant exist")    
